611281f2d25d9a8167315a2f24f2efe1ee99af39
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-08-17 09:22:15 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Comp :obj="obj" :obj2="obj" :obj3="obj" />
+  </div>
+</template>
+
+<script>
+import Comp from "./components/Component.vue";
+
+export default {
+  components: {
+    Comp
+  },
+  setup() {
+    const obj = {
+      get [Symbol.toStringTag]() {
+        return "NotObject";
+      }
+    };
+
+    return { obj };
+  }
+};
+</script>
---
+++ src/components/Component.vue
@@ -0,0 +1,14 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    obj: Object,
+    obj2: { type: Object },
+    obj3: { validator: o => typeof o === "object" }
+  }
+};
+</script>
